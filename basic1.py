614d74c05087c6494f07eeb6602a701b7dfe4b98
박대희 <59219339+peogml@users.noreply.github.com>
2020-01-05 19:01:47 +0900

    Update basic1.py

--- basic1.py
+++ basic1.py
@@ -1,7 +1,7 @@
 #직원들의 급여를 5%인상하고 거주 지역을 파악하는 코드
 
 SALARY_RAISE_FACTOR=0.05 # 5%인상하기 위한 변수
-STATE_CODE_MAP = {'WA': 'Washington', 'TX': 'Texas'} # key와 value를 할당하는 문법
+STATE_CODE_MAP = {'WA': 'Washington', 'TX': 'Texas'} # key와 value를 할당하는 문법{}
 
 def update_employee_record(rec): # def는 define. 즉 정의하는 것. rec은 딕셔너리를 사용하는 매개변수.
     old_sal = rec['salary']
@@ -44,5 +44,9 @@ print("abc"[1])
 print("abcd"[1:3])
 
 my_list = ["a","b","c"]
+
 my_set = set(my_list)
 my_tuple = tuple(my_list)
+
+print(my_list)
+
